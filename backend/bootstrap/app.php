d670320bd72e0496e3cc0e90a72e2999616253e1
Mauricelio Pereira Silvestre <138402158+Mauricelio-Pereira@users.noreply.github.com>
2026-04-17 20:50:32 -0300

    feat: backend - implementar os arquivos de doc

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,17 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->group('api', [
             \App\Http\Middleware\Cors::class,
-            \App\Http\Middleware\EnsureAcceptsJson::class,
+            \App\Http\Middleware\EnsureAcceptsJson::class
         ]);
 
         $middleware->alias([
-            'check.password.reset.token' => \App\Http\Middleware\CheckPasswordResetToken::class,
-            'ensures.json' => \App\Http\Middleware\EnsureAcceptsJson::class,
+            'ensures.json' => \App\Http\Middleware\EnsureAcceptsJson::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
